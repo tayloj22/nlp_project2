4e0d4c616ecaaa167105e227e98a213a528bdc35
Thomas Westra <twestra@Thomass-MacBook-Pro-3.local>
2020-03-30 20:58:32 -0400

    Added bigram MLE, as well as unigram model building

--- d1.py
+++ d1.py
@@ -31,19 +31,54 @@ def unigramMLE(unigram, corpus):
         for j in range(len(corpus[i])):
             #Increment totalCount for each word in corpus
             totalCount += 1
-            if (unigram is corpus[i][j]):
+            if (unigram == corpus[i][j]):
                 #Increment wordCount each time the word is found in corpus
                 wordCount += 1
-    
+                
     unigramProb = wordCount/totalCount
     
     return unigramProb
 
+#Function to build our model with probabilities of each unigram
+def unigramModel(corpus):
+    model = []
+    inModel = False
+    #First, go through and build a list of our possible unigrams
+    for i in range(len(corpus)):
+        for j in range(len(corpus[i])):
+            tempWord = corpus[i][j]
+            #check that our model isn't empty so it doesn't go out of bounds
+            if model:
+                for k in range(len(model)):
+                    if (tempWord == model[k][0]):
+                        inModel = True
+            #If our word isn't in our model yet, we can first calculate it's MLE and then add it
+            if not inModel:
+                tempProb = unigramMLE(tempWord, corpus)
+                model.append((tempWord, tempProb))
+    return model
+                
+                    
+            
+#Function that calculate MLE for a bigram
+def bigramMLE(bigram, corpus):
+    phraseCount = 0
+    totalCount = 0
+    
+    for i in range(len(corpus)):
+        for j in (range(len(corpus[i])-1)):
+            #Increment totalcount for each two word phrase in corpus
+            totalCount += 1
+            if(bigram == (' '.join([corpus[i][j], corpus[i][j+1]]))):
+                #Increment phraseCount each time the phrase is found in corpus
+                phraseCount += 1
+    print(totalCount)
+    print(phraseCount)
+    bigramProb = phraseCount/totalCount
+    
+    return bigramProb
+            
 
-
-#Function that returns Max Liklihood Estimation for a bigram
-def bigramMLE(bigram):
-    return bigram
 
 
 
@@ -51,38 +86,39 @@ def bigramMLE(bigram):
 if __name__ == "__main__":
     #Ask the user for the filename associated with the corpus
     print("Provide the filename, with extension, to the desired corpus.")
-    corpusFile = raw_input()
+    corpusFile = input()
     #Calls upon parseCorpus to extract the words from the corpus
     corpus = parseCorpus(corpusFile)
-
-    print("Type 1 for unigram probability, or type 2 for bigram probability.")
-    gramVal = input()
-    
-    #Directs the program to the unigramMLE calculation
-    if (gramVal == 1):
-        print("Enter the unigram of your choice.")
-        #Take the unigram from the user
-        unigram = raw_input()
-        #Strip it of the newline character
-        unigram = unigram.strip()
-        
-        #Call on unigramMLE function
-        unigramProb = unigramMLE(unigram, corpus)
-        print 'The unsmoothed unigram probability estimate is', unigramProb
-    
-    #Directs the program to the bigramMLE calculation
-    elif (gramVal == 2):
-        print("Enter the bigram of your choice.")
-        #Take the bigram from the user
-        bigram = raw_input('\n')
-        #Strip it of the newline character
-        bigram = bigram.strip('\n')
-        
-        #Call on bigramMLE function
-        bigramProb = bigramMLE(bigram, corpus)
-        print 'The unsmoothed bigram probability estimate is', bigramProb
-     
-    
-    #Error handling for incorrect user input
-    else:
-        print("User input not recognized.")
+    print(corpus)
+    print(unigramModel(corpus))
+#    print("Type 1 for unigram probability, or type 2 for bigram probability.")
+#    gramVal = input()
+#
+#    #Directs the program to the unigramMLE calculation
+#    if (gramVal == "1"):
+#        print("Enter the unigram of your choice.")
+#        #Take the unigram from the user
+#        unigram = input()
+#        #Strip it of the newline character
+#        unigram = unigram.strip()
+#
+#        #Call on unigramMLE function
+#        unigramProb = unigramMLE(unigram, corpus)
+#        print ('The unsmoothed unigram probability estimate is', unigramProb)
+#
+#    #Directs the program to the bigramMLE calculation
+#    elif (gramVal == "2"):
+#        print("Enter the bigram of your choice.")
+#        #Take the bigram from the user
+#        bigram = input()
+#        #Strip it of the newline character
+#        bigram = bigram.strip('\n')
+#
+#        #Call on bigramMLE function
+#        bigramProb = bigramMLE(bigram, corpus)
+#        print ('The unsmoothed bigram probability estimate is', bigramProb)
+#
+#
+#    #Error handling for incorrect user input
+#    else:
+#        print("User input not recognized.")
